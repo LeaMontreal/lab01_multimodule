f545ba03a65b04734c97bdeb371cdef72b32bbb2
wanglijun <wanglijun00123172@gmail.com>
2021-10-10 17:08:08 -0400

    L02_activity 代码微小调整

--- app/l02_activity/src/main/java/com/wlj/l02_activity/ReplyActivity.java
+++ app/l02_activity/src/main/java/com/wlj/l02_activity/ReplyActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 public class ReplyActivity extends AppCompatActivity {
 
     private EditText et_user_reply;
-    private Button btn_return_noreply;
-    private Button btn_return_withreply;
+//    private Button btn_return_noReply;
+//    private Button btn_return_withReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +21,28 @@ public class ReplyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_reply);
 
         et_user_reply = findViewById(R.id.et_user_reply);
-        btn_return_noreply = (Button) findViewById(R.id.btn_return_noreply);
-        btn_return_withreply = (Button) findViewById(R.id.btn_return_withreply);
+        // 通过在布局xml文件中加入单击响应函数的方式设置监听，确实不需要再声明和初始化Button对象了
+//        btn_return_noReply = (Button) findViewById(R.id.btn_return_noReply);
+//        btn_return_withReply = (Button) findViewById(R.id.btn_return_withReply);
 
+        // 获取启动当前界面的Activity带过来的信息
         Intent intent = getIntent();
         String msg = intent.getStringExtra("MESSAGE");
+
+        // 处理intent中的信息，此处显示在EditText View中
         et_user_reply.setText(msg);
 
         // 设置监听，方式三
-        // 在layout.xml中通过android:onClick="return_withreply"加入处理button单击操作的响应函数
+        // 在layout.xml中通过android:onClick="return_withReply"加入处理button单击操作的响应函数
     }
 
     // 在XxxxActivity中声明这个方法，统一都是public void xxx(View view){};
-    public void return_noreply(View view) {
+    public void return_noReply(View view) {
         // 直接返回
         finish();
     }
 
-    public void return_withreply(View view) {
+    public void return_withReply(View view) {
         //Toast.makeText(this, "Execute return with reply", Toast.LENGTH_LONG).show();
 
         // 1.准备返回结果code
